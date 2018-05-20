f6c9d7c13e7c885e5e017d1b15d10a2d18bedf66
ChunHLi <chunhli@cs.stonybrook.edu>
2018-05-20 07:29:58 -0700

    Added accordian

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -34,3 +34,18 @@ $('a[href*="#"]')
       }
     }
   });
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("clicked");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
